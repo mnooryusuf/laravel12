3d70dd325ac37cad0da3d2594f461f3b4a5c44b4
Muhammad Noor Yusuf <mnyusuf14@gmail.com>
2025-07-07 10:41:13 +0930

    start today

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Contact</h1>
+    <ul>
+        <li>
+            <a href="https/instagram.com/mn_ysf14">Instagram</a>
+        </li>
+        <li>
+            <a href="https/x.com/">X</a>
+        </li>
+        <li>
+            <a href="https/youtube.com/">Youtube</a>
+        </li>
+    </ul>
+</body>
+
+</html>
